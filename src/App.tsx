5b5e282430a85488e8f288f171119d479d16ac77
vde-guil <valentin.de.guillebon@gmail.com>
2022-01-17 12:12:37 +0100

    type app component thanks to generics

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
+interface Person {
+  id: number,
+  email: string,
+  avatar: string,
+}
+
 function App() {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState<Person[]>([]);
   useEffect(() => {
     const fetchImages = async () => {
-      const data = (await axios.get("https://reqres.in/api/users/")).data.data;
+      const data = (await axios.get("https://reqres.in/api/users/")).data.data as Person[];
       setPersons(data);
     };
     fetchImages();
@@ -16,7 +22,7 @@ function App() {
       {persons.map((person) => (
         <React.Fragment key={person.id}>
           <p>{person.email}</p>
-          <img src={person.avatar} />
+          <img src={person.avatar} alt="person avatar" />
         </React.Fragment>
       ))}
     </div>
